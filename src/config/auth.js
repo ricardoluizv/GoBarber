aa71eea6d81b816d7eb91ff111c6d98a51f57ddf
Ricardo Luiz Vieira <ricardoluizv@gmail.com>
2019-09-15 23:12:55 -0300

    chore: It was created register user, user validatio, session and routes

---
+++ src/config/auth.js
@@ -0,0 +1,6 @@
+// https://www.md5online.org/ : gerar um hash
+// senha utilizada no md5 online: 1** ***OK
+export default {
+  secret: 'ccd28d479ffbc41dd253c37a0c3c5822',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,20 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Diego Fernandes',
-    email: 'diego@rocketseat.com.br',
-    password_hash: '1238712387',
-  });
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
-  return res.json(user);
-});
+// Adicionar o middleare de forma local:
+// routes.put('/users', authMiddleware, UserController.update);
+
+// Adiciona o middleware de forma global, a partir desta linha estará disponçiveis as demais rotas, antes disso não funcionará
+routes.use(authMiddleware);
+
+// De forma global não especifica o middleware nos parâmetros de .put
+routes.put('/users', UserController.update);
 
 export default routes;
